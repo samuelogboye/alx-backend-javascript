b321475a9256e1f5b58566fe03b462adf65631ff
Samuel Ogboye <ogboyesam@gmail.com>
2024-01-04 02:30:15 +0100

    iterate through

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employeeNames = '';
-  for (const [index, item] of Object.entries(reportWithIterator)) {
-    employeeNames += `${item}`;
-
-    if (parseInt(index) !== reportWithIterator.length - 1) {
-      employeeNames += ' | ';
-    }
-  }
-
-  return employeeNames;
+  return reportWithIterator.join(' | ');
 }
