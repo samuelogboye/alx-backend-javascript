752b515635e3c330e51cdb8c72c32468e3211fa2
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-28 23:49:31 +0100

    update express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 
-function countStudents (path) {
+function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
-      if (err) return reject(Error('Cannot load the database'));
+    fs.readFile(path, { encoding: "utf-8" }, (err, data) => {
+      if (err) return reject(Error("Cannot load the database"));
       // split data and taking only list without header
-      const lines = data.split('\n').slice(1, -1);
+      const lines = data.split("\n").slice(1, -1);
       // give the header of data
-      const header = data.split('\n').slice(0, 1)[0].split(',');
+      const header = data.split("\n").slice(0, 1)[0].split(",");
       // find firstname and field index
-      const idxFn = header.findIndex((ele) => ele === 'firstname');
-      const idxFd = header.findIndex((ele) => ele === 'field');
+      const idxFn = header.findIndex((ele) => ele === "firstname");
+      const idxFd = header.findIndex((ele) => ele === "field");
       // declarate two dictionaries for count each fields and store list of students
       const fields = {};
       const students = {};
@@ -19,10 +19,10 @@ function countStudents (path) {
       const all = {};
 
       lines.forEach((line) => {
-        const list = line.split(',');
+        const list = line.split(",");
         if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
         fields[list[idxFd]] += 1;
-        if (!students[list[idxFd]]) students[list[idxFd]] = '';
+        if (!students[list[idxFd]]) students[list[idxFd]] = "";
         students[list[idxFd]] += students[list[idxFd]]
           ? `, ${list[idxFn]}`
           : list[idxFn];
@@ -46,16 +46,16 @@ function countStudents (path) {
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+app.get("/", (req, res) => {
+  res.send("Hello Holberton School!");
 });
 
-app.get('/students', (req, res) => {
-  res.write('This is the list of our students');
+app.get("/students", (req, res) => {
+  res.write("This is the list of our students\n");
   countStudents(process.argv[2])
     .then((data) => {
       res.write(data.numberStudents);
-      res.end(data.listStudents.join('\n'));
+      res.end(data.listStudents.join("\n"));
     })
     .catch((err) => {
       res.end(err.message);
