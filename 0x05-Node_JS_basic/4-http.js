528f32ec82226c37500ad5b4b6d99235d5375d47
Samuel Ogboye <ogboyesam@gmail.com>
2024-02-28 23:13:48 +0100

    more

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,12 @@
-const http = require("http");
+const http = require('http');
 
-const hostname = "127.0.0.1";
+const hostname = '127.0.0.1';
 const port = 1245;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello Holberton School!\n");
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 server.listen(port, hostname);
